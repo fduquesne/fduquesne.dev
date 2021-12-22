563fcd076957e11157f9a5cb70f8b4274f0e22dc
Florian Duquesne <fduquesne@icloud.com>
2021-12-22 05:54:57 +0100

    feat(blog): write my first blog page

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -16,8 +16,8 @@ const PostDetailPage = ({ post }) => {
   });
 
   const customComponents = {
-    h2: heading => <h2 className="py-3 text-2xl md:text-3xl font-semibold">{heading.children}</h2>,
-    h3: heading => <h3 className="py-3 text-xl md:text-2xl font-semibold">{heading.children}</h3>,
+    h2: heading => <h2 className="pt-8 pb-3 text-2xl md:text-3xl font-semibold">{heading.children}</h2>,
+    h3: heading => <h3 className="pt-8 pb-3 text-xl md:text-2xl font-semibold">{heading.children}</h3>,
     p: paragraph => <p className="py-3">{paragraph.children}</p>,
     a: link => (
       <a className="text-indigo-400 hover:underline cursor-pointer" href={link.href} target="_blank" rel="noreferrer">
@@ -30,6 +30,11 @@ const PostDetailPage = ({ post }) => {
       <li className="pl-2 mb-3 text-gray-500">
         <div className="text-gray-900 dark:text-gray-200">{item.children}</div>
       </li>
+    ),
+    pre: pre => (
+      <pre className="p-3 border border-gray-300 rounded-md text-gray-500 dark:border-gray-700 dark:text-gray-400">
+        {pre.children}
+      </pre>
     ),
     blockquote: blockquote => (
       <blockquote className="pl-5 border-l-2 border-gray-300 text-gray-500 dark:border-gray-700 dark:text-gray-400 italic">
